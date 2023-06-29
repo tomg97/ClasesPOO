8f01e67109be041052c47f59a0c168824ee21c13
tomg97 <64111181+tomg97@users.noreply.github.com>
2023-06-28 21:31:09 -0300

    Add files via upload

---
+++ clase2806POO/clase2806POO/Entidades/Jugador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase2806POO.Entidades
+{
+    public class Jugador
+    {
+        public delegate void delFestejo(string mensaje);
+        public delegate void delInsulto(string mensaje, int volumen);
+
+        public event delFestejo festejo;
+        public event delInsulto insulto;
+
+        Random random = new Random();
+
+        public void patear()
+        {
+            int resultado = random.Next(0, 2);
+            switch (resultado)
+            {
+                case 0:
+                    {
+                        if(festejo != null)
+                        {
+                            festejo("Gol");
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        if(insulto != null)
+                        {
+                            insulto("asdadssa", 10);
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ clase2806POO/clase2806POO/Form1.cs
@@ -0,0 +1,39 @@
+﻿using clase2806POO.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clase2806POO
+{
+    public partial class Form1 : Form
+    {
+        Jugador jugador = new Jugador();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPatada_Click(object sender, EventArgs e)
+        {
+            jugador.festejo += festejar;
+            jugador.insulto += insultar;
+            jugador.patear();
+        }
+
+        private void insultar(string mensaje, int volumen)
+        {
+            lblAccion.Text = mensaje + " " + volumen.ToString();
+        }
+
+        private void festejar(string mensaje)
+        {
+            lblAccion.Text = mensaje;
+        }
+    }
+}
